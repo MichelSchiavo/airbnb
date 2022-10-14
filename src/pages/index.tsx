2df1e68f9ae5158e32f8453aa16cdffcc9a28e09
Michel Felipe <michel_dmc@hotmail.com>
2022-10-14 19:20:57 -0300

    Add banner feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { Header } from "components/Header";
+import { Banner } from "components/Banner";
 // import Image from "next/image";
 
 const Home: NextPage = () => {
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+      <Banner />
     </div>
   );
 };
